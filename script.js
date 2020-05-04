e52d68a4e1036f853ec646c4d6e61fa79732eace
barnettx <barnett.lee.zhi.le@gmail.com>
2020-05-04 21:12:14 +0800

    modified the banner so that my face doesn't clip

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function openMobileNav(){
     document.querySelector('.mobile__menu').style.width = "100%";
     document.querySelector('.nav__mobile__open').style.display = "none";
     document.querySelector('.nav__mobile__close').style.display = "block";
+    document.querySelector('body').style.overflowY = "hidden";
 }
 
 function closeMobileNav(){
@@ -10,5 +11,6 @@ function closeMobileNav(){
     document.querySelector('.mobile__menu').style.width = "0%";
     document.querySelector('.nav__mobile__open').style.display = "block";
     document.querySelector('.nav__mobile__close').style.display = "none";
+    document.querySelector('body').style.overflowY = "scroll";
 }
 
